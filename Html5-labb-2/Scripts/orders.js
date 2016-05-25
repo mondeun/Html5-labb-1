4c60045c23f317f051812db107beac8e224b4577
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-05-25 16:26:35 +0200

    Got basic js working to present data

--- Html5-labb-2/Scripts/orders.js
+++ Html5-labb-2/Scripts/orders.js
@@ -1,1 +1,19 @@
-﻿
+﻿/*
+ * Thanks to André Snede Hansen for coming up with this snippet.
+ * I have modded it to replace + sign in text to spaces.
+ * http://stackoverflow.com/questions/647259/javascript-query-string
+ */
+
+// use location search in parameter to acquire url data.
+function searchToObject(search) {
+    var pairs = search.substring(1).split("&"),
+        obj = {}, pair;
+
+    for (var i in pairs) {
+        if (pairs[i] === "") continue;
+        pair = pairs[i].split("=");
+        pair[1] = pair[1].replace(/\+/g, " ");
+        obj[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+    }
+    return obj;
+}
